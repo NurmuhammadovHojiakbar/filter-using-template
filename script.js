539922485bb51c4e37f83cd30015a564201b4570
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2021-08-12 18:10:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let filterCards = {
+    checkboxes: [
+        {
+            name:"phone",
+            text: "Modern Smartphones",
+        },
+        {
+            name:"computer",
+            text: "Desktop Computers",
+        },
+        {
+            name:"laptop",
+            text: "Laptops",
+        },
+        {
+            name:"camera",
+            text: "Cameras",
+        },
+        {
+            name:"computer-tools",
+            text: "Computer Tools",
+        },
+        {
+            name:"home-technics",
+            text: "Home Technics",
+        }
+    ],
+    cards:[
+        
+    ]
+}
